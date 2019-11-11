aad4b4db0a7810eebf08105613539d956b04b2bb
Brian Sorahan <bsorahan@gmail.com>
2019-11-11 13:30:32 -0600

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/briansorahan/iowa
+
+go 1.12
+
+require github.com/pkg/errors v0.8.1
---
+++ main.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+func main() {
+	config, err := NewConfig()
+	die(errors.Wrap(err, "parsing config"))
+	app, err := NewApp(config)
+	die(errors.Wrap(err, "initializing app"))
+	die(app.Run(context.Background()))
+}
+
+// App defines the application's behavior.
+type App struct {
+	Config
+}
+
+// NewApp initializes the application.
+func NewApp(conf Config) (*App, error) {
+	app := &App{Config: conf}
+
+	return app, nil
+}
+
+// Run runs the application.
+func (a *App) Run(ctx context.Context) error {
+	if a.Download {
+		return a.download(ctx)
+	}
+	return a.list(ctx)
+}
+
+func (a *App) download(ctx context.Context) error {
+	return nil
+}
+
+func (a *App) list(ctx context.Context) error {
+	return nil
+}
+
+// Config defines the application's configuration.
+type Config struct {
+	All      bool   `json:"all"`
+	Download bool   `json:"download"`
+	Era      string `json:"era"`
+	Section  string `json:"section"`
+
+	// Samples is a map from "era" (i.e. pre-2012, post-2012) to "section"
+	// (e.g. brass, percussion, woodwind) to the list of URL's that
+	// contain the sample download links.
+	Samples map[string]map[string][]string `json:"samples"`
+}
+
+// NewConfig parses the application's configuration from env/flags.
+func NewConfig() (Config, error) {
+	config := Config{
+		Samples: map[string]map[string][]string{
+			"pre-2012": {
+				"woodwind": {
+					"http://theremin.music.uiowa.edu/MISflute.html",
+					"http://theremin.music.uiowa.edu/MISaltoflute.html",
+					"http://theremin.music.uiowa.edu/MISbassflute.html",
+					"http://theremin.music.uiowa.edu/MISoboe.html",
+					"http://theremin.music.uiowa.edu/MISEbclarinet.html",
+					"http://theremin.music.uiowa.edu/MISBbclarinet.html",
+					"http://theremin.music.uiowa.edu/MISbassclarinet.html",
+					"http://theremin.music.uiowa.edu/MISbassoon.html",
+					"http://theremin.music.uiowa.edu/MISsopranosaxophone.html",
+					"http://theremin.music.uiowa.edu/MISaltosaxophone.html",
+				},
+				"brass": {
+					"http://theremin.music.uiowa.edu/MISFrenchhorn.html",
+					"http://theremin.music.uiowa.edu/MISBbtrumpet.html",
+					"http://theremin.music.uiowa.edu/MIStenortrombone.html",
+					"http://theremin.music.uiowa.edu/MISbasstrombone.html",
+					"http://theremin.music.uiowa.edu/MIStuba.html",
+				},
+				"strings": {
+					"http://theremin.music.uiowa.edu/MISviolin.html",
+					"http://theremin.music.uiowa.edu/MISviola.html",
+					"http://theremin.music.uiowa.edu/MIScello.html",
+					"http://theremin.music.uiowa.edu/MISdoublebass.html",
+					"http://theremin.music.uiowa.edu/MISviolin2012.html",
+					"http://theremin.music.uiowa.edu/MISviola2012.html",
+					"http://theremin.music.uiowa.edu/MIScello2012.html",
+					"http://theremin.music.uiowa.edu/MISdoublebass2012.html",
+				},
+				"percussion": {
+					"http://theremin.music.uiowa.edu/Mismarimba.html",
+					"http://theremin.music.uiowa.edu/MISxylophone.html",
+					"http://theremin.music.uiowa.edu/Misvibraphone.html",
+					"http://theremin.music.uiowa.edu/MISbells.html",
+					"http://theremin.music.uiowa.edu/MIScrotales.html",
+					"http://theremin.music.uiowa.edu/MISgongtamtams.html",
+					"http://theremin.music.uiowa.edu/MIShandpercussion.html",
+					"http://theremin.music.uiowa.edu/MIStambourines.html",
+				},
+				"piano/other": {
+					"http://theremin.music.uiowa.edu/MISpiano.html",
+					"http://theremin.music.uiowa.edu/MISballoonpop.html",
+					"http://theremin.music.uiowa.edu/MISguitar.html",
+				},
+				"foundobjects": {
+					"http://theremin.music.uiowa.edu/MISfoundobjects1.html",
+				},
+			},
+			"post-2012": {
+				"woodwinds": {
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISFlute2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISaltoflute2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBassFlute2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISOboe2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISEbClarinet2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbClarinet2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbBassClarinet2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBassoon2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbSopranoSaxophone2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISEbAltoSaxophone2012.html",
+				},
+				"brass": {
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISHorn2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbTrumpet2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISTenorTrombone2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBassTrombone2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISTuba2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbBassClarinet2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBassoon2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBbSopranoSaxophone2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISEbAltoSaxophone2012.html",
+				},
+				"strings": {
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISViolin2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISViola2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISCello2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISDoubleBass2012.html",
+				},
+				"percussion": {
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISMarimba2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISxylophone2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISVibraphone2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISBells2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISCrotales2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISCymbals2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISGongsTamTams2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISHandPercussion2012.html",
+					"http://theremin.music.uiowa.edu/MIS-Pitches-2012/MISTambourines2012.html",
+				},
+				"foundobjects": {
+					"http://theremin.music.uiowa.edu/MISfoundobjects2.html",
+				},
+			},
+		},
+	}
+	flag.BoolVar(&config.All, "a", false, "Download/list all the samples. Defaults to true")
+	flag.BoolVar(&config.Download, "dl", false, "Download samples (default is to just print a JSON list to stdout).")
+	flag.StringVar(&config.Era, "post-2012", false, "Filter by era ('all', 'pre-2012', 'post-2012').")
+	flag.StringVar(&config.Section, "s", "", "(REQUIRED) Section (e.g. brass, woodwind, percussion")
+	flag.Parse()
+
+	if config.Era != "all" {
+		sections, ok := config.Samples[config.Era]
+		if !ok {
+			return config, errors.New("unsupported era: " + config.Era)
+		}
+	}
+	return config, nil
+}
+
+func die(err error) {
+	if err == nil {
+		return
+	}
+	fmt.Fprintln(os.Stderr, err.Error())
+	os.Exit(1)
+}
